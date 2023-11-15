adff4571f9277b2e7afc8219815b5a2137d3cd49
Nicolle Policiano <nicolle.fabri@gmail.com>
2023-11-14 19:53:07 -0600

    Control onboarding state

--- Capstone/ContentView.swift
+++ Capstone/ContentView.swift
@@ -9,26 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var path = NavigationPath()
-    @State var isShowingUserName = false
-    @State var isShowingHome = false
+    @State private var isShowingUserName = false
+    @State private var isShowingHome = false
+    @State private var onboardingStatus: OnboardingStatus = .pendingWalkthrough
     
     var body: some View {
-        OnboardingView(onStart: {
-            isShowingUserName = true
+        switch onboardingStatus {
+        case .pendingWalkthrough:
+            OnboardingView(onStart: {
+                isShowingUserName = true
+            })
+            .fullScreenCover(isPresented: $isShowingUserName) {
+                userNameView
+            }
+        case .pendingName:
+            userNameView
+        case .pendingPreferences:
+            NavigationStack {
+                userDetailView
+            }
+        case .complete:
+            HomeView()
+        }
+    }
+    
+    // MARK: Screens
+    
+    private var userDetailView: some View {
+        UserDetailView(onStart: {
+            isShowingHome = true
         })
-        .fullScreenCover(isPresented: $isShowingUserName) {
-            NavigationStack(path: $path) {
-                UserNameView(onStart: {
-                    path.append(Destination.userDetail)
-                })
-                .navigationDestination(for: Destination.self) { destination in
-                    UserDetailView(onStart: {
-                        isShowingHome = true
-                    })
-                    .fullScreenCover(isPresented: $isShowingHome) {
-                        HomeView()
-                    }
-                }
+        .fullScreenCover(isPresented: $isShowingHome) {
+            HomeView()
+        }
+    }
+    
+    private var userNameView: some View {
+        NavigationStack(path: $path) {
+            UserNameView(onStart: {
+                path.append(Destination.userDetail)
+            })
+            .navigationDestination(for: Destination.self) { destination in
+                userDetailView
             }
         }
     }
@@ -37,6 +59,13 @@ struct ContentView: View {
 enum Destination: Hashable {
     case userDetail
     case newRecipe
+}
+
+enum OnboardingStatus {
+    case pendingWalkthrough
+    case pendingName
+    case pendingPreferences
+    case complete
 }
 
 struct ContentView_Previews: PreviewProvider {
