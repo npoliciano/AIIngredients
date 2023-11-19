80232a94b632fa534cc1656627207545973e315c
Nicolle Policiano <nicolle.fabri@gmail.com>
2023-11-18 23:15:51 -0600

    Create HTTP client that is able to execute POST requests

---
+++ Capstone/API/URLSessionHTTPClient.swift
@@ -0,0 +1,39 @@
+import Foundation
+
+struct HTTPStatusError: Error { }
+
+protocol HTTPClient {
+    func post(from url: URL, body: Data) async throws -> Data
+}
+
+final class URLSessionHTTPClient: HTTPClient {
+    private let urlSession: URLSession
+    private let authorizationKey: String
+    
+    init(
+        urlSession: URLSession = .shared,
+        authorizationKey: String
+    ) {
+        self.urlSession = urlSession
+        self.authorizationKey = authorizationKey
+    }
+    
+    func post(from url: URL, body: Data) async throws -> Data {
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "POST"
+        urlRequest.httpBody = body
+        urlRequest.allHTTPHeaderFields = [
+            "Authorization": "Bearer \(authorizationKey)",
+            "Content-Type": "application/json"
+        ]
+        
+        let (data, response) = try await urlSession.data(for: urlRequest)
+        
+        guard let response = response as? HTTPURLResponse,
+              (200 ... 299).contains(response.statusCode)  else {
+            throw HTTPStatusError()
+        }
+        
+        return data
+    }
+}
